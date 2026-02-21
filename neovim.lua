44b63c88f0c76cb88e47dd621c162f0632a44af6
Luca Pattocchio <pattocchioluca@gmail.com>
2026-02-21 22:49:44 +0100

    Add LazyVim configuration to neovim.lua
    Updated neovim.lua to include LazyVim configuration.

--- neovim.lua
+++ neovim.lua
@@ -1,8 +1,12 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require("rose-pine").setup({})
-		vim.cmd("colorscheme rose-pine")
-	end,
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "rose-pine",
+		},
+	},
 }
